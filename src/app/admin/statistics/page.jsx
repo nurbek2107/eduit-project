72fd86344f705079367f6c5a8f28d8bb2adfe687
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-07-16 00:19:33 +0500

    main
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/app/admin/statistics/page.jsx
+++ src/app/admin/statistics/page.jsx
@@ -1,11 +1,12 @@
 
 import { SidebarPie } from "@/components";
+import { LineChart } from "@/components";
 
 function Users() {
   return (
     <section className="p-5 flex flex-col gap-10">
       <SidebarPie />
-
+      <LineChart />
     </section>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,5 @@ export { default as SidebarList } from './SidebarList'
 export { default as ExportClients } from './ExportClients'
 export { default as UserData } from './UserData'
 export { default as SidebarPie } from './SidebarPie'
-
+export { default as LineChart } from './LineChart'
 
