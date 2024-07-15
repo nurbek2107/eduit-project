9372f73293a5f2e153e0e04d580498d8df9667b0
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-07-15 22:58:29 +0500

    main
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/app/admin/statistics/page.jsx
+++ src/app/admin/statistics/page.jsx
@@ -7,6 +7,7 @@ function Users() {
 
         <SidebarPie />
 
+
     </section>
   );
 }
--- src/components/SidebarPie.jsx
+++ src/components/SidebarPie.jsx
@@ -1,42 +1,46 @@
 "use client";
-import { Bar } from "react-chartjs-2";
-import Chart from "chart.js/auto";
-const VulnChart = () => {
-  return (
-    <div>
-      <Bar
-        data={{
-          labels: [
-            "Sqli",
-            "XSS",
-            "XXE",
-            "Open Redirect",
-            "Broken Authentication",
-          ],
-          datasets: [
-            {
-              label: "# of vulnerabilities",
-              data: [15, 12, 6, 7, 4],
-              backgroundColor: ["red", "yellow", "blue", "black", "green"],
-              borderColor: "orange",
-              borderWidth: 5,
-            },
-            {
-              label: "Web Apps",
-              data: [20, 13, 6, 8, 9],
-              backgroundColor: "purple",
-              borderColor: "red",
-              borderWidth: 5,
-            },
-          ],
-        }}
-        height={300}
-        width={500}
-        options={{
-          maintainAspectRatio: false,
-        }}
-      />
-    </div>
-  );
+
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic"; // Import dynamic from Next.js for dynamic imports
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+} from "@material-tailwind/react";
+import { Square3Stack3DIcon } from "@heroicons/react/24/outline";
+
+// Dynamically import Chart from react-apexcharts
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const chartConfig = {
+  type: "pie",
+  width: 280,
+  height: 280,
+  series: [44, 55, 13, 43, 22],
+  options: {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+    },
+    title: {
+      show: "",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    colors: ["#020617", "#ff8f00", "#00897b", "#1e88e5", "#d81b60"],
+    legend: {
+      show: false,
+    },
+  },
 };
-export default VulnChart;
+
+export default function Example() {
+  return (
+    <CardBody className="mt-4 grid place-items-center px-2">
+      <Chart {...chartConfig} />
+    </CardBody>
+  );
+}
