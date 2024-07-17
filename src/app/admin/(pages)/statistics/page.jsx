b14eb41ccd418d677642b47e61aa90bb5671eafc
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-07-17 14:38:29 +0500

    main
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/app/admin/(pages)/statistics/page.jsx
+++ src/app/admin/(pages)/statistics/page.jsx
@@ -4,7 +4,7 @@ import { LineChart } from "../../../../components";
 
 function Users() {
   return (
-    <section className="p-5 flex flex-col gap-10">
+    <section className="p-5  flex flex-col gap-10">
       <SidebarPie />
       <LineChart />
     </section>
